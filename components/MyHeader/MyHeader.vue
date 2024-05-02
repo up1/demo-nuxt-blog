fb4c856273a21f5c7e9a86c271ca08d106dd2528
up1 <somkiat.p@gmail.com>
2024-05-02 11:12:04 +0700

    Start demo

---
+++ components/MyHeader/MyHeader.vue
@@ -0,0 +1,40 @@
+<template>
+    <header class="top-0 bg-white z-10 p-0.5">
+      <nav class="py-2 px-4">
+        <div class="container mx-auto px-4 flex justify-between items-center">
+          <div class="flex items-center">
+            <MyHeaderAppLogo />
+          </div>
+          <div class="menu">
+            <MyHeaderGuestMenu />
+          </div>
+        </div>
+      </nav>
+    </header>
+  </template>
+
+<style scoped>
+@media (min-width: 544px) {
+  .container {
+    max-width: 576px;
+  }
+}
+
+@media (min-width: 768px) {
+  .container {
+    max-width: 720px;
+  }
+}
+
+@media (min-width: 992px) {
+  .container {
+    max-width: 940px;
+  }
+}
+
+@media (min-width: 1200px) {
+  .container {
+    max-width: 1140px;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Head>
+      <title>Home — Demo</title>
+    </Head>
+    <AppHome />
+    <MyContainer>
+      <div class="flex md:space-x-4">
+        <div class="md:w-3/4 mr-4">
+          List of blogs
+        </div>
+        <div class="md:w-1/4 mt-2">
+          <div class="sidebar p-3 rounded bg-gray-100">
+            <p class="mb-2 text-sm font-medium">Popular Tags</p>
+          </div>
+        </div>
+      </div>
+    </MyContainer>
+  </div>
+</template>
