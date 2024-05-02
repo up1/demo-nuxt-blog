d952114b18bafdfcecd6d15260d59b88b3f50957
up1 <somkiat.p@gmail.com>
2024-05-02 12:43:19 +0700

    Starter project

---
+++ components/FavoriteButton.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps<{
+  favoritesCount: number;
+}>();
+</script>
+
+<template>
+  <div>
+    <button class="flex items-center px-2 py-1 text-sm font-medium bg-custom-green text-white rounded-full">
+      <span class="inline-flex">
+        <IconHeart color="currentColor" class="w-3 h-3 mr-1" />
+      </span>
+      <div>{{ props.favoritesCount }}</div>
+    </button>
+  </div>
+</template>
---
+++ types/index.ts
@@ -0,0 +1,9 @@
+export interface Blog {
+    id: number;
+    title: string;
+    description: string;
+    tags: string[];
+    username: string;
+    createdDate: string;
+    favoritesCount: number;
+}
